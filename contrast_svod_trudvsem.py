c038d34c3bc811a059b7194c6909f8bb21bf3533
Oleg Budaev <t0r1n88@mail.ru>
2024-05-03 10:56:51 +0800

    Изменил механизм формирования листа остлеживания динамики

--- contrast_svod_trudvsem.py
+++ contrast_svod_trudvsem.py
@@ -4,7 +4,7 @@
 from support_functions import write_df_to_excel_color_selection # получаем функцию для записи в листы Excel
 import pandas as pd
 from tkinter import messagebox
-
+import math
 import time
 import openpyxl
 from openpyxl.styles import Font, PatternFill
@@ -64,6 +64,21 @@ def convert_int(value):
         return 0
 
 
+def union_column(row):
+    """
+    Функция для преобразования двух колонок получившихся после мерджа в одну
+    """
+    out_value = None
+    first_value = str(row[0])
+    second_value = str(row[1])
+    lst_value = [value for value in (row[0],row[1]) if not math.isnan(value)]
+    print(lst_value)
+
+
+
+    print(out_value)
+    print(2/0)
+
 
 
 def prepare_diff_svod_trudvsem(first_file:str, second_file:str, end_folder:str,type_contrast:str):
@@ -87,8 +102,8 @@ def prepare_diff_svod_trudvsem(first_file:str, second_file:str, end_folder:str,t
                            'Квоты по работодателям','Вакансии для соц.кат.',
                            'Требуемый опыт по отраслям', 'Требуемый опыт по работодателям']
 
-        lst_not_standard_sheets = ['Зарплата по отраслям',
-                                   'Зарплата по работодателям']  # список листов без колонки Количество вакансий
+        lst_not_standard_sheets = ['Вакансии для динамики','Зарплата по отраслям',
+                                   'Зарплата по работодателям']  # список листов требующих нестандартной обработки
 
         dct_df = dict() # создаем словарь для хранения датафреймов
 
@@ -162,90 +177,110 @@ def prepare_diff_svod_trudvsem(first_file:str, second_file:str, end_folder:str,t
                         (merge_df['Вторая таблица'] / merge_df['Первая таблица']) * 100, 2)
 
                     merge_df.sort_values(by='Показатель', inplace=True)  # Сортируем по показателю
-                    if name_sheet != 'Вакансии для динамики':
-                        dct_df[name_sheet] = merge_df  # сохраняем в словарь
-                    else: # Обрабатываем отдельно лист Вакансии для динамики, чтобы Работодатель и Вакансии были по отдельности
-                        merge_df['_Работодатель'] = merge_df['Показатель'].apply(lambda x: split_cell(x, '+&+', 0))
-                        merge_df['_Вакансия'] = merge_df['Показатель'].apply(lambda x: split_cell(x, '+&+', 1))
-
-                        merge_df.insert(0, 'Работодатель', merge_df['_Работодатель'])
-                        merge_df.insert(1, 'Вакансия', merge_df['_Вакансия'])
-
-                        merge_df.drop(columns=['Показатель', '_Работодатель', '_Вакансия'], inplace=True)
-                        dct_df[name_sheet] = merge_df  # сохраняем в словарь
+                    dct_df[name_sheet] = merge_df  # сохраняем в словарь
+                    # if name_sheet != 'Вакансии для динамики':
+                    #     dct_df[name_sheet] = merge_df  # сохраняем в словарь
+                    # else: # Обрабатываем отдельно лист Вакансии для динамики, чтобы Работодатель и Вакансии были по отдельности
+                    #     merge_df['_Работодатель'] = merge_df['Показатель'].apply(lambda x: split_cell(x, '+&+', 0))
+                    #     merge_df['_Вакансия'] = merge_df['Показатель'].apply(lambda x: split_cell(x, '+&+', 1))
+                    #
+                    #     merge_df.insert(0, 'Работодатель', merge_df['_Работодатель'])
+                    #     merge_df.insert(1, 'Вакансия', merge_df['_Вакансия'])
+                    #
+                    #     merge_df.drop(columns=['Показатель', '_Работодатель', '_Вакансия'], inplace=True)
+                    #     dct_df[name_sheet] = merge_df  # сохраняем в словарь
 
                 else:
-                    # обрабатываем нестандартные листы
-                    first_df = pd.read_excel(first_file, sheet_name=name_sheet)  # первый файл для сравнения
-                    second_df = pd.read_excel(second_file, sheet_name=name_sheet)  # второй файл для сравнения
+                    # вычисляем динамику изменения вакансий
+                    if name_sheet == 'Вакансии для динамики':
+                        first_df = pd.read_excel(first_file, sheet_name=name_sheet)  # первый файл для сравнения
+                        second_df = pd.read_excel(second_file, sheet_name=name_sheet)  # второй файл для сравнения
+                        # Проводим внешнее слияние
+                        merge_df = first_df.merge(second_df, how='outer', left_on=['ID вакансии'], right_on=['ID вакансии'], indicator=True)
+                        #merge_df.drop(columns=['ID вакансии','_merge'],inplace=True) # удаляем лишние колонки
+                        # Создаем объединенные колонки
+                        merge_df['Работодатель'] = merge_df[['Полное название работодателя_x','Полное название работодателя_y']].apply(union_column,axis=1)
 
-                    # получаем колонки которые будем использовать для создания ID
-                    first_key_columns = list(first_df.columns)
-                    if 'Средняя ариф. минимальная зп' not in first_key_columns or 'Медианная минимальная зп' not in first_key_columns: # проверяем наличие колонки с вакансиями
-                        raise NotColumnPay
-                    first_key_columns.remove('Средняя ариф. минимальная зп')
-                    first_key_columns.remove('Медианная минимальная зп')
-
-                    second_key_columns = list(second_df.columns)
-                    if 'Средняя ариф. минимальная зп' not in second_key_columns or 'Медианная минимальная зп' not in second_key_columns: # проверяем наличие колонки с вакансиями
-                        raise NotColumnPay
-                    second_key_columns.remove('Средняя ариф. минимальная зп')
-                    second_key_columns.remove('Медианная минимальная зп')
-
-                    # Создаем колонки по которым будет вестись объединение
-                    first_df['ID'] = first_df[first_key_columns].apply(lambda x: sum_category(x, '-'), axis=1)
-                    second_df['ID'] = second_df[second_key_columns].apply(lambda x: sum_category(x, '-'), axis=1)
-
-                    # Проводим внешнее слияние
-                    merge_df = first_df.merge(second_df, how='outer', left_on=['ID'], right_on=['ID'], indicator=True)
-
-                    # удаляем лишние колонки
-                    del_columns = [column for column in merge_df.columns if ('минимальная зп' not in column) & ('ID' not in column)]
-                    merge_df.drop(columns=del_columns, inplace=True)
-
-                    # Упорядочиваем колонки
-                    merge_df = merge_df.reindex(
-                        columns=['ID', 'Средняя ариф. минимальная зп_x','Средняя ариф. минимальная зп_y', 'Медианная минимальная зп_x',
-                                 'Медианная минимальная зп_y'])  # меняем местами
-                    merge_df.columns = ['Показатель', 'Средняя ариф. минимальная. Первая таблица', 'Средняя ариф. минимальная. Вторая таблица',
-                                        'Медианная минимальная. Первая таблица','Медианная минимальная. Вторая таблица']
-
-                    merge_df.fillna(0, inplace=True) # заполняем наны
-
-                    merge_df[['Средняя ариф. минимальная. Первая таблица', 'Средняя ариф. минимальная. Вторая таблица',
-                                        'Медианная минимальная. Первая таблица','Медианная минимальная. Вторая таблица']] = merge_df[['Средняя ариф. минимальная. Первая таблица', 'Средняя ариф. минимальная. Вторая таблица',
-                                        'Медианная минимальная. Первая таблица','Медианная минимальная. Вторая таблица']].applymap(convert_int) # приводик колонки с числами к int
-
-                    # Создаем колонки с подчетом разниц ср.ариф.
-                    merge_df['Сред.ариф.мин. Разница'] = merge_df['Средняя ариф. минимальная. Вторая таблица'] - merge_df['Средняя ариф. минимальная. Первая таблица']
-                    merge_df['Сред.ариф.мин. Абсолютная разница'] = abs(merge_df['Средняя ариф. минимальная. Вторая таблица'] - merge_df['Средняя ариф. минимальная. Первая таблица'])
-                    merge_df['Сред.ариф.мин. Изменение в %'] = round(
-                        ((merge_df['Средняя ариф. минимальная. Вторая таблица'] - merge_df['Средняя ариф. минимальная. Первая таблица']) / merge_df['Средняя ариф. минимальная. Первая таблица']) * 100, 2)
-
-                    merge_df['Сред.ариф.мин. Отношение второй таблицы к первой %'] = round(
-                        (merge_df['Средняя ариф. минимальная. Вторая таблица'] / merge_df['Средняя ариф. минимальная. Первая таблица']) * 100, 2)
-
-                    # Создаем колонки с подсчетом разниц медиан
-                    merge_df['Медианнная.мин. Разница'] = merge_df['Медианная минимальная. Вторая таблица'] - merge_df[
-                        'Медианная минимальная. Первая таблица']
-
-                    merge_df['Медианнная.мин. Абсолютная разница'] = abs(
-                        merge_df['Медианная минимальная. Вторая таблица'] - merge_df[
-                            'Медианная минимальная. Первая таблица'])
-
-                    merge_df['Медианнная.мин. Изменение в %'] = round(
-                        ((merge_df['Медианная минимальная. Вторая таблица'] - merge_df[
-                            'Медианная минимальная. Первая таблица']) / merge_df[
-                             'Медианная минимальная. Первая таблица']) * 100, 2)
-
-                    merge_df['Медианнная.мин. Отношение второй таблицы к первой %'] = round(
-                        (merge_df['Медианная минимальная. Вторая таблица'] / merge_df[
-                            'Медианная минимальная. Первая таблица']) * 100, 2)
-                    #
+                        print(merge_df.columns)
 
 
-                    merge_df.sort_values(by='Показатель', inplace=True)  # Сортируем по показателю
-                    dct_df[name_sheet] = merge_df  # сохраняем в словарь
+                        merge_df.to_excel('data/fdsfd.xlsx',index=False)
+
+
+
+
+                    else:
+                        # обрабатываем листы с зарплатой
+                        first_df = pd.read_excel(first_file, sheet_name=name_sheet)  # первый файл для сравнения
+                        second_df = pd.read_excel(second_file, sheet_name=name_sheet)  # второй файл для сравнения
+
+                        # получаем колонки которые будем использовать для создания ID
+                        first_key_columns = list(first_df.columns)
+                        if 'Средняя ариф. минимальная зп' not in first_key_columns or 'Медианная минимальная зп' not in first_key_columns: # проверяем наличие колонки с вакансиями
+                            raise NotColumnPay
+                        first_key_columns.remove('Средняя ариф. минимальная зп')
+                        first_key_columns.remove('Медианная минимальная зп')
+
+                        second_key_columns = list(second_df.columns)
+                        if 'Средняя ариф. минимальная зп' not in second_key_columns or 'Медианная минимальная зп' not in second_key_columns: # проверяем наличие колонки с вакансиями
+                            raise NotColumnPay
+                        second_key_columns.remove('Средняя ариф. минимальная зп')
+                        second_key_columns.remove('Медианная минимальная зп')
+
+                        # Создаем колонки по которым будет вестись объединение
+                        first_df['ID'] = first_df[first_key_columns].apply(lambda x: sum_category(x, '-'), axis=1)
+                        second_df['ID'] = second_df[second_key_columns].apply(lambda x: sum_category(x, '-'), axis=1)
+
+                        # Проводим внешнее слияние
+                        merge_df = first_df.merge(second_df, how='outer', left_on=['ID'], right_on=['ID'], indicator=True)
+
+                        # удаляем лишние колонки
+                        del_columns = [column for column in merge_df.columns if ('минимальная зп' not in column) & ('ID' not in column)]
+                        merge_df.drop(columns=del_columns, inplace=True)
+
+                        # Упорядочиваем колонки
+                        merge_df = merge_df.reindex(
+                            columns=['ID', 'Средняя ариф. минимальная зп_x','Средняя ариф. минимальная зп_y', 'Медианная минимальная зп_x',
+                                     'Медианная минимальная зп_y'])  # меняем местами
+                        merge_df.columns = ['Показатель', 'Средняя ариф. минимальная. Первая таблица', 'Средняя ариф. минимальная. Вторая таблица',
+                                            'Медианная минимальная. Первая таблица','Медианная минимальная. Вторая таблица']
+
+                        merge_df.fillna(0, inplace=True) # заполняем наны
+
+                        merge_df[['Средняя ариф. минимальная. Первая таблица', 'Средняя ариф. минимальная. Вторая таблица',
+                                            'Медианная минимальная. Первая таблица','Медианная минимальная. Вторая таблица']] = merge_df[['Средняя ариф. минимальная. Первая таблица', 'Средняя ариф. минимальная. Вторая таблица',
+                                            'Медианная минимальная. Первая таблица','Медианная минимальная. Вторая таблица']].applymap(convert_int) # приводик колонки с числами к int
+
+                        # Создаем колонки с подчетом разниц ср.ариф.
+                        merge_df['Сред.ариф.мин. Разница'] = merge_df['Средняя ариф. минимальная. Вторая таблица'] - merge_df['Средняя ариф. минимальная. Первая таблица']
+                        merge_df['Сред.ариф.мин. Абсолютная разница'] = abs(merge_df['Средняя ариф. минимальная. Вторая таблица'] - merge_df['Средняя ариф. минимальная. Первая таблица'])
+                        merge_df['Сред.ариф.мин. Изменение в %'] = round(
+                            ((merge_df['Средняя ариф. минимальная. Вторая таблица'] - merge_df['Средняя ариф. минимальная. Первая таблица']) / merge_df['Средняя ариф. минимальная. Первая таблица']) * 100, 2)
+
+                        merge_df['Сред.ариф.мин. Отношение второй таблицы к первой %'] = round(
+                            (merge_df['Средняя ариф. минимальная. Вторая таблица'] / merge_df['Средняя ариф. минимальная. Первая таблица']) * 100, 2)
+
+                        # Создаем колонки с подсчетом разниц медиан
+                        merge_df['Медианнная.мин. Разница'] = merge_df['Медианная минимальная. Вторая таблица'] - merge_df[
+                            'Медианная минимальная. Первая таблица']
+
+                        merge_df['Медианнная.мин. Абсолютная разница'] = abs(
+                            merge_df['Медианная минимальная. Вторая таблица'] - merge_df[
+                                'Медианная минимальная. Первая таблица'])
+
+                        merge_df['Медианнная.мин. Изменение в %'] = round(
+                            ((merge_df['Медианная минимальная. Вторая таблица'] - merge_df[
+                                'Медианная минимальная. Первая таблица']) / merge_df[
+                                 'Медианная минимальная. Первая таблица']) * 100, 2)
+
+                        merge_df['Медианнная.мин. Отношение второй таблицы к первой %'] = round(
+                            (merge_df['Медианная минимальная. Вторая таблица'] / merge_df[
+                                'Медианная минимальная. Первая таблица']) * 100, 2)
+                        #
+
+
+                        merge_df.sort_values(by='Показатель', inplace=True)  # Сортируем по показателю
+                        dct_df[name_sheet] = merge_df  # сохраняем в словарь
 
 
 
@@ -451,10 +486,10 @@ def prepare_diff_svod_trudvsem(first_file:str, second_file:str, end_folder:str,t
 
 if __name__ == '__main__':
     main_first_file = 'data/Аналитика по вакансиям региона/15_03_2024/Свод по региону Бурятия от 14 марта.xlsx'
-    main_first_file = 'data/Красноярский край/Аналитика по вакансиям выбранных работодателей/25_03_2024/Свод по выбранным работодателям от 15_09_09.xlsx'
+    main_first_file = 'data/Республика Бурятия/Аналитика по вакансиям региона/03_05_2024/Свод по региону от 08_37_42.xlsx'
     main_second_file = 'data/Аналитика по вакансиям региона/16_03_2024/Свод по региону Бурятия от 16 марта.xlsx'
     main_second_file = 'data/Аналитика по вакансиям региона/19_03_2024/Свод по региону от 09_53_17.xlsx'
-    main_second_file = 'data/Красноярский край/25.05/Аналитика по вакансиям выбранных работодателей/25_03_2024/Свод по выбранным работодателям от 15_11_13.xlsx'
+    main_second_file = 'data/Республика Бурятия/Аналитика по вакансиям региона/03_05_2024/Свод из будущего.xlsx'
     main_end_folder = 'data'
 
     prepare_diff_svod_trudvsem(main_first_file, main_second_file, main_end_folder,'No')
