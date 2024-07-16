2a9e7d0ac86cf435238adf5f82787a661c43cf3c
Oleg Budaev <t0r1n88@mail.ru>
2024-07-16 09:50:02 +0800

    Реализовал создание словаря с данными и функции для проверки корректности сохранения данных

--- mon_grad_check_functions.py
+++ mon_grad_check_functions.py
@@ -1,12 +1,58 @@
 """
 Модуль для проверочных функций мониторинга занятости выпускников для сайта СССР
 """
-import pandas as pd
-import numpy as np
+from check_functions import extract_code_nose
 import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
 
-import re
-import os
 
 
-def
+def create_check_tables_mon_grad(high_level_dct: dict):
+    """
+    Функция для создания файла с данными по каждой специальности чтобы можно было проверить правильность внесенных
+    данных
+    """
+    # Создаем словарь в котором будут храниться словари по специальностям
+    code_spec_dct = {}
+    # инвертируем словарь так, чтобы код специальности стал внешним ключом а названия файлов внутренними
+    for poo, spec_data in high_level_dct.items():
+        for code_spec, data in spec_data.items():
+            if code_spec not in code_spec_dct:
+                code_spec_dct[code_spec] = {f'{poo}': high_level_dct[poo][code_spec]}
+            else:
+                code_spec_dct[code_spec].update({f'{poo}': high_level_dct[poo][code_spec]})
+    # Сортируем получившийся словарь по возрастанию для удобства использования
+    sort_code_spec_dct = sorted(code_spec_dct.items())
+    code_spec_dct = {dct[0]: dct[1] for dct in sort_code_spec_dct}
+
+    # Создаем файл
+    wb = openpyxl.Workbook()
+    # Создаем листы
+    for idx, code_spec in enumerate(code_spec_dct.keys()):
+        if code_spec != 'nan':
+            code = extract_code_nose(code_spec)
+            wb.create_sheet(title=code, index=idx)
+
+    for code_spec in code_spec_dct.keys():
+        if code_spec != 'nan':
+            code = extract_code_nose(code_spec)
+            # Конвертируем в датафрейм
+            temp_code_df = pd.DataFrame.from_dict(code_spec_dct[code_spec], orient='index')
+            # Удаляем колонки без названий
+            temp_code_df.drop(columns=[name_column for name_column in temp_code_df.columns if 'Unnamed' in name_column],
+                              inplace=True)
+
+            temp_code_df = temp_code_df.reset_index()  # извлекаем название организации
+            temp_code_df.rename(columns={'index': 'Наименование'},
+                                inplace=True)  # переименовываем колонку с названием организации
+
+            for r in dataframe_to_rows(temp_code_df, index=False, header=True):
+                wb[code].append(r)
+            wb[code].column_dimensions['A'].width = 20
+            wb[code].column_dimensions['B'].width = 40
+
+    # Удаляем лист Sheet
+    if 'Sheet' in wb.sheetnames:
+        del wb['Sheet']
+    return wb
--- monitoring_graduate_employment.py
+++ monitoring_graduate_employment.py
@@ -2,8 +2,10 @@
 """
 Модуль для обработки таблиц мониторинга занятости выпускников используемого для загрузки на сайт СССР
 """
-from check_functions import base_check_file
+from check_functions import base_check_file,extract_code_nose
 from support_functions import convert_to_int
+
+from mon_grad_check_functions import create_check_tables_mon_grad
 
 import pandas as pd
 import numpy as np
@@ -11,7 +13,6 @@ import os
 import warnings
 from tkinter import messagebox
 import time
-
 pd.options.mode.chained_assignment = None  # default='warn'
 warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 warnings.filterwarnings('ignore', category=DeprecationWarning)
@@ -34,7 +35,7 @@ def prepare_graduate_employment(path_folder_data: str, path_result_folder: str):
     requred_columns_first_sheet = ['1', '1.1', '1.2', '2', '3', '3.1', '3.2', '4', '5', '6', '7', '8', '9', '10',
                                    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
                                    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-                                   '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+                                   '31', '32','32.1','32.2', '33', '34', '35', '36', '37', '38', '39', '40',
                                    '41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
                                    '51', '52', '53', '54', '55', '56', '57', '58', '59', '60',
                                    '61', '62', '63', '64', '65', '66', '67', '68', '69', '70',
@@ -64,6 +65,7 @@ def prepare_graduate_employment(path_folder_data: str, path_result_folder: str):
                 if len(file_error_df) != 0:
                     continue
                 print(file)
+                name_file = file.split('.xlsx')[0]
                 # Обрабатываем данные с листа Выпуск-СПО
                 df_first_sheet = pd.read_excel(f'{path_folder_data}/{file}', sheet_name='Выпуск-СПО',
                                                skiprows=check_required_dct['Выпуск-СПО']['Количество строк заголовка']) # Считываем прошедший базовую проверку файл
@@ -73,16 +75,53 @@ def prepare_graduate_employment(path_folder_data: str, path_result_folder: str):
                 df_first_sheet[requred_columns_first_sheet[1:]] = df_first_sheet[requred_columns_first_sheet[1:]].applymap(convert_to_int)
                 # очищаем первую колонку от пробельных символов вначаче и конце
                 df_first_sheet['1'] = df_first_sheet['1'].apply(lambda x:x.strip() if isinstance(x,str) else x)
-                # Проверяем заполнение колонки 1
+                # TODO Проверки файлов
+                # Проверяем правильность заполнения колонки 1
+                df_first_sheet['Код'] = df_first_sheet['1'].apply(extract_code_nose)  # очищаем от текста в кодах
+                if 'error' in df_first_sheet['Код'].values:
+                    temp_error_df = pd.DataFrame(data=[[f'{name_file}', '',
+                                                        'Некорректные значения в колонке 1 Код и наименование профессии/специальности.Вместо кода присутствует дата, и т.п. проверьте правильность заполнения колонки 1!!!']],
+                                                 columns=['Название файла', 'Строка или колонка с ошибкой',
+                                                          'Описание ошибки'])
+                    error_df = pd.concat([error_df, temp_error_df], axis=0, ignore_index=True)
+                    continue
+
+                df_first_sheet.drop(columns=['Код'],inplace=True)
 
 
+                # Заполняем словарь данными
+                # перебираем список словарей
+                # Создание словаря для хранения данных файла
+                code_spec = [spec for spec in df_first_sheet['1'].unique()]  # получаем список специальностей которые есть в файле
+                # Создаем словарь нижнего уровня содержащий в себе все данные для каждой специальности
+                spec_dict = {}
+                for spec in code_spec:
+                    spec_dict[spec] = {key:0 for key in requred_columns_first_sheet}
 
+                high_level_dct[name_file] = copy.deepcopy(spec_dict)
+                first_sheet_lst_dct = df_first_sheet.to_dict(orient='records')
 
+                # добавляем данные
+                for dct in first_sheet_lst_dct:
+                    high_level_dct[name_file][dct['1']].update(dct)
 
-
-        print(error_df)
         t = time.localtime()  # получаем текущее время
         current_time = time.strftime('%H_%M_%S', t)
+
+        wb_check_tables = create_check_tables_mon_grad(high_level_dct)  # проверяем данные по каждой специальности
+        wb_check_tables.save(
+            f'{path_result_folder}/Данные для проверки правильности заполнения файлов от {current_time}.xlsx')
+
+
+
+
+
+
+        # print(error_df)
+
+
+
+
         error_df.to_excel(f'{path_result_folder}/Ошибки {current_time}.xlsx',index=False)
     except ZeroDivisionError:
         print('dssd')
