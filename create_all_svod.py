10fed475982d2e10f775df080377ead3a93d035b
Oleg Budaev <t0r1n88@mail.ru>
2025-12-23 15:27:26 +0800

    начало работы над соединением сводов

--- create_all_svod.py
+++ create_all_svod.py
@@ -1,6 +1,8 @@
 """
 Скрипт для создания временных рядов из сводных таблиц созданных Кассандрой
 """
+import re
+
 import openpyxl
 import pandas as pd
 import os
@@ -27,6 +29,79 @@ class NotReqColumns(Exception):
 
 
 
+def preparing_data(data_folder:str,required_columns:dict,dct_index_svod:dict,error_df:pd.DataFrame):
+    """
+    Функция для проверки исходных файлов на базовые ошибки и создания списков встречающихся индексов (первой колонки)
+    """
+    for dirpath, dirnames, filenames in os.walk(data_folder):
+        for file in filenames:
+            if not file.startswith('~$') and (file.endswith('.xlsx') or file.endswith('.xlsm')):
+                try:
+                    if file.endswith('.xlsx'):
+                        name_file = file.split('.xlsx')[0].strip()
+                    else:
+                        name_file = file.split('.xlsm')[0].strip()
+                    # проверяем на правильность даты в названии
+                    result_date = re.search(r'\d{2}_\d{2}_\d{4}', name_file)
+                    if result_date:
+                        file_date = result_date.group()
+                        file_date = file_date.replace('_', '.')
+                    else:
+                        temp_error_df = pd.DataFrame(
+                            data=[[f'{name_file}',
+                                   f'В названии файла отсутствует дата в правильном формате. Требуется формат DD_MM_YYYY т.е. 25.12.2025'
+                                   ]],
+                            columns=['Название файла',
+                                     'Описание ошибки'])
+                        error_df = pd.concat([error_df, temp_error_df], axis=0,
+                                             ignore_index=True)
+                        continue
+                    # открываем файл для проверки наличия листов и колонок
+                    temp_wb = openpyxl.load_workbook(f'{dirpath}/{file}', read_only=True)
+                    temp_wb_sheets = set(temp_wb.sheetnames)
+                    diff_sheets = set(required_columns.keys()).difference(set(temp_wb_sheets))
+                    if len(diff_sheets) != 0:
+                        temp_error_df = pd.DataFrame(
+                            data=[[f'{name_file}',
+                                   f'Отсутствуют обязательные листы {diff_sheets}'
+                                   ]],
+                            columns=['Название файла',
+                                     'Описание ошибки'])
+                        error_df = pd.concat([error_df, temp_error_df], axis=0,
+                                             ignore_index=True)
+                        continue
+
+                    # Собираем возможные индексы которые могут встретиться
+                    for sheet, lst_cols in required_columns.items():
+                        temp_req_df = pd.read_excel(f'{dirpath}/{file}', sheet_name=sheet)
+                        diff_cols = set(lst_cols).difference(set(temp_req_df.columns))
+                        if len(diff_cols) != 0:
+                            temp_error_df = pd.DataFrame(
+                                data=[[f'{name_file}',
+                                       f'На листе {sheet} отсутствуют обязательные колонки {diff_cols}'
+                                       ]],
+                                columns=['Название файла',
+                                         'Описание ошибки'])
+                            error_df = pd.concat([error_df, temp_error_df], axis=0,
+                                                 ignore_index=True)
+                            continue
+
+                        # Открываем файл для обработки
+                        df = pd.read_excel(f'{dirpath}/{file}', sheet_name=sheet)  # открываем файл
+                        dct_index_svod[sheet].update(df[df.columns[0]])
+                except:
+                    temp_error_df = pd.DataFrame(
+                        data=[[f'{name_file}',
+                               f'Не удалось обработать файл. Возможно файл поврежден'
+                               ]],
+                        columns=['Название файла',
+                                 'Описание ошибки'])
+                    error_df = pd.concat([error_df, temp_error_df], axis=0,
+                                         ignore_index=True)
+                    continue
+
+
+
 
 
 
@@ -45,7 +120,6 @@ def processing_time_series(data_folder:str,end_folder:str):
     current_time = time.strftime('%H_%M_%S', t)
     current_date = time.strftime('%d_%m_%Y', t)
     # Обязательные листы
-    required_sheets = {'Вакансии по отраслям','Вакансии по муниципалитетам','Зарплата по отраслям'}
     error_df = pd.DataFrame(
         columns=['Название файла', 'Описание ошибки'])  # датафрейм для ошибок
 
@@ -62,7 +136,8 @@ def processing_time_series(data_folder:str,end_folder:str):
         required_columns = {'Вакансии по отраслям':['Сфера деятельности','Количество вакансий'],
                             'Вакансии по муниципалитетам':['Муниципалитет','Количество вакансий'],
                             'Зарплата по отраслям':['Сфера деятельности','Средняя ариф. минимальная зп','Медианная минимальная зп']}
-
+        dct_index_svod = {key:set() for key in required_columns.keys()} # словарь для хранения всех значений сводов которые могут встретиться в файлах
+        preparing_data(data_folder,required_columns,dct_index_svod,error_df) #
         for dirpath, dirnames, filenames in os.walk(data_folder):
             for file in filenames:
                 if not file.startswith('~$') and (file.endswith('.xlsx') or file.endswith('.xlsm')):
@@ -72,32 +147,17 @@ def processing_time_series(data_folder:str,end_folder:str):
                         else:
                             name_file = file.split('.xlsm')[0].strip()
                         print(name_file)  # обрабатываемый файл
-                        temp_wb = openpyxl.load_workbook(f'{dirpath}/{file}',read_only=True)
-                        temp_wb_sheets = set(temp_wb.sheetnames)
-                        diff_sheets = required_sheets.difference(set(temp_wb_sheets))
-                        if len(diff_sheets) != 0:
-                            temp_error_df = pd.DataFrame(
-                                data=[[f'{name_file}',
-                                       f'Отсутствуют обязательные листы {diff_sheets}'
-                                       ]],
-                                columns=['Название файла',
-                                         'Описание ошибки'])
-                            error_df = pd.concat([error_df, temp_error_df], axis=0,
-                                                 ignore_index=True)
-                            continue
 
 
-                        df = pd.read_excel(f'{dirpath}/{file}', dtype=str)  # открываем файл
-                    except:
-                        temp_error_df = pd.DataFrame(
-                            data=[[f'{name_file}',
-                                   f'Не удалось обработать файл. Возможно файл поврежден'
-                                   ]],
-                            columns=['Название файла',
-                                     'Описание ошибки'])
-                        error_df = pd.concat([error_df, temp_error_df], axis=0,
-                                             ignore_index=True)
-                        continue
+
+
+
+
+
+
+
+
+
 
         error_df.to_excel(f'{end_folder}/Ошибки_{current_time}.xlsx',index=False)
 
